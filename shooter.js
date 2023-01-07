7f70945ab08ad391a4e66e5038cbfe627b5993bb
Dany Cervantes <danycervantesq@gmail.com>
2023-01-07 13:33:20 -0500

    Update shooter.js

--- shooter.js
+++ shooter.js
@@ -349,7 +349,7 @@ class Torpedo {
                 fireVy = this.y - obj.y; //user y velocity set at impact
                 
                 //blue blood;
-                \\this.color = "blue";
+                //this.color = "blue";
                 this.target = { 
                     x: Math.cos(Math.PI * 2 + randomRange(-10,10)) * Math.random(), //creates circular particle positions
                     y: Math.sin(Math.PI * 2 + randomRange(-10,10)) * Math.random() //creates curved particle positions
